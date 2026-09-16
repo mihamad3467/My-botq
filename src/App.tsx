3d21ddbbabcd3fa373effa0704e7cc747a2741c3
mihamad3467 <jjh65249@gmail.com>
2026-09-16 17:56:12 +0300

    Create App component for Quran Bot Showcase

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Quran Bot Showcase</h1>
+      <p>الموقع قيد التشغيل بنجاح!</p>
+    </div>
+  );
+}
