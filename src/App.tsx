889f73c40c7734c18c7390bc2a45515445ffc10f
mihamad3467 <jjh65249@gmail.com>
2026-09-16 17:56:35 +0300

    Delete src/App.tsx

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
-      <h1>Quran Bot Showcase</h1>
-      <p>الموقع قيد التشغيل بنجاح!</p>
-    </div>
-  );
-}
